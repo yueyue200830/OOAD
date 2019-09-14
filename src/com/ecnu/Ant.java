394e251135812ece828d4480d9431e415f7505ce
momo7773 <47842212+momo7773@users.noreply.github.com>
2019-09-14 15:38:57 +0800

    Complete Ant Class

--- src/com/ecnu/Ant.java
+++ src/com/ecnu/Ant.java
@@ -1,4 +1,58 @@
 package com.ecnu;
 
 public class Ant {
+    private double velocity;
+    private boolean isLeft;
+    private double postion;
+    private boolean isAlive;
+    
+    Ant(double veolocity,boolean isLeft,double position){
+        this.velocity = velocity;
+        this.isLeft = isLeft;
+        this.postion = position;
+        this.isAlive = true;
+    }
+    public double  getVelocity(){
+        return this.velocity;
+    }
+
+    public boolean isLeft() {
+        return isLeft;
+    }
+
+    public double getPostion() {
+        return postion;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void changeDirection() {
+        if (this.isLeft) {
+            this.isLeft = false;
+        } else {
+            this.isLeft = true;
+        }
+    }
+    
+    public void step_straight(double timeInterval) {
+        if (this.isLeft) {
+            this.postion -= this.velocity * timeInterval;
+        } else {
+            this.postion += this.velocity * timeInterval;
+        }
+    }
+    
+    public void step_collision(double timeInterval, double timeConsume) {
+        if(this.isLeft) {
+            this.postion = this.postion + timeConsume * this.velocity - (timeInterval - timeConsume) * this.velocity;
+        } else {
+            this.postion = this.postion - timeConsume * this.velocity + (timeInterval - timeConsume) * this.velocity;
+        }
+    }
+    
+    public void setDie() {
+        this.isAlive = false;
+    }
 }
--- src/com/ecnu/GameControl.java
+++ src/com/ecnu/GameControl.java
@@ -1,4 +1,15 @@
 package com.ecnu;
 
 public class GameControl {
+    private int numberAnt;
+    private double[] velocityGroup;
+    private double timeInterval;
+    GameControl(int numberAnt, double[] velocityGroup, double timeInterval) {
+        this.numberAnt = numberAnt;
+        this.velocityGroup = velocityGroup;
+        this.timeInterval = timeInterval;
+    }
+    public void enumerateGame() {
+        
+    }
 }
