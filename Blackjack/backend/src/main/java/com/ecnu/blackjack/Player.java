2886fd82925665fce85f7a1beddc214bc46dc790
yueyue200830 <yueyue200830@163.com>
2019-10-21 13:24:25 +0800

    Update Player.java

--- Blackjack/backend/src/main/java/com/ecnu/blackjack/Player.java
+++ Blackjack/backend/src/main/java/com/ecnu/blackjack/Player.java
@@ -12,15 +12,15 @@ public class Player {
     private int bet;
 
 
-    Player () {
+    Player() {
         this.hand = new Hand();
     }
 
-    public int getBet () {
+    public int getBet() {
         return this.bet;
     }
 
-    public void setBet (int bet) {
+    public void setBet(int bet) {
         this.bet = bet;
     }
 
@@ -28,26 +28,44 @@ public class Player {
      * Add a new card
      * @param newCard The new card to be added to the hand
      */
-    public void receiveCard (Card newCard) {
+    public void receiveCard(Card newCard) {
         this.hand.addCard(newCard);
     }
+
     /**
      * Get a list of the card in the hand.
      * @return A copy list of the cards.
      */
-    public List<Card> getCard () {
+    public List<Card> getCard() {
         return this.hand.getCards();
+    }
+
+    /**
+     * Get the sum of cards that is visible to dealer.
+     * @return The sum of visible cards.
+     */
+    public int tellCardSumToDealer() {
+        List<Card> cards = this.hand.getCards();
+        int sum = 0;
+        for (Card card : cards) {
+            if (card.isVisible()) {
+                sum += card.getCardNumber();
+            }
+        }
+        return sum;
     }
 
     /**
      * Calculate the sum of cards.
      * @return The sum of cards
      */
-    public int getCardSum () {
+    public int getCardSum() {
         return this.hand.getSum();
     }
 
-    public boolean getLoseOrNot() {return this.hand.isLargerThan21();}
+    public boolean getLoseOrNot() {
+        return this.hand.isLargerThan21();
+    }
 
     public int getCardNumber() {
         return this.hand.getCardNumber();
