261a3d2adcdd24fca6d19c220ab70fd7900171e2
yueyue200830 <45792349+yueyue200830@users.noreply.github.com>
2019-10-27 12:44:15 +0800

    Update App.js

--- Blackjack/fontend/src/App.js
+++ Blackjack/fontend/src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 
-function App() {
-  return (
-      <div className="App">
-          <div className="Player-part">
-              Player Page
-          </div>
-          <div className="Information-part">
-              other information
-          </div>
-      </div>
-  );
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <div className="Player-part">
+                    Player Page
+                </div>
+                <div className="Information-part">
+                    other information
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
