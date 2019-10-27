ed05daaa38ab0051c8045575129ccb38707d106f
momo7773 <47842212+momo7773@users.noreply.github.com>
2019-10-27 20:29:53 +0800

    complete draw, pass, double

--- Blackjack/backend/src/main/java/com/ecnu/blackjack/Controller.java
+++ Blackjack/backend/src/main/java/com/ecnu/blackjack/Controller.java
@@ -1,4 +1,27 @@
 package com.ecnu.blackjack;
 
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.stereotype.Controller
 public class Controller {
+
+    @RequestMapping(value = "/drawCard",method = RequestMethod.GET)
+    @ResponseBody
+    public int getOneCard(@RequestParam(name="playerNo") int playerNo,@RequestParam(name = "handNo") int handNo){
+        System.out.println("receive " + playerNo + " " + handNo);
+        int[] numberOfHand = {1,2,3};
+        Deck deck = new Deck(3, numberOfHand);
+        int cardNum = deck.basicDraw(1,1,true);
+        return cardNum;
+    }
+
+    @RequestMapping(value = "/doubleBet",method = RequestMethod.GET)
+    @ResponseBody
+    public String doubleBet(@RequestParam(name = "playerNo") int playerNo,@RequestParam(name = "handNo") int handNo){
+        System.out.println("receive " + playerNo + " " + handNo);
+        int[] numberOfHand = {1,2,3};
+        Deck deck = new Deck(3, numberOfHand);
+        deck.doubleBet(1,1);
+        return "Success";
+    }
 }
--- Blackjack/fontend/src/App.js
+++ Blackjack/fontend/src/App.js
@@ -1,17 +1,179 @@
 import React from 'react';
 import './App.css';
+import { Button,Divider } from 'antd';
 import axios from 'axios';
+import 'antd/dist/antd.css';
 
 class App extends React.Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            playerNumber: 3,
+            playerHandNumber: [1,1],
+            turn: 1,
+            playerStatus: [true],
+            initialDraw: true,
+            cardInfo: [[[2,16,48]],[[11,14,50]]],
+            currentPlayer: 1,
+            currentHand: 1,
+            betList:[[10,12],[20]],
+        };
+    }
+    drawCard= () => {
+        console.log("start draw");
+        axios.get('/drawCard',{
+            params:{playerNo:1,handNo: 1}
+        })
+            .then(
+                res => {
+                    console.log(res.data);
+                    this.state.cardInfo[0][0].push(res.data);
+                    this.forceUpdate();
+                }
+                )
+            .catch(
+                res => {
+                    console.log("error");
+                }
+            )
+    }
+    revertCard= (cardNumber) => {
+        console.log("card number " + cardNumber);
+        let color = Math.floor((cardNumber + 1) / 13);
+        let value = cardNumber % 13;
+        let description = "";
+        //console.log("color: " + color);
+       // console.log("value: " + value);
+        switch (color) {
+            case 0:
+                console.log("color = 0");
+                description += "Clover " + value;
+                break;
+            case 1:
+                console.log("color = 1");
+                description += "Spade " + value;
+                break;
+            case 2:
+                console.log("color = 2");
+                description += "diamond " + value;
+                break;
+            case 3:
+                console.log("color = 3");
+                description += "Heart " + value;
+                break;
+        }
+        description += ".jpg";
+        //console.log("transfer over " + description);
+        return description;
+    }
+    displayCard= (card,index) => {
+        let imgSrc = this.revertCard(card);
+        console.log("load " + imgSrc);
+        return(
+            <div className="Each-Card" >
+                <img width="120px" height="200px" src={require("./Card/" + imgSrc)}>
+                </img>
+            </div>
+        )
+}
+    doubleBet = ()=> {
+        let betList = this.state.betList;
+        console.log("double bet");
+        this.setState({
+           betList: this.state.betList[0][0]*2}
+        )
+        this.forceUpdate();
+        axios.get("/doubleBet",{
+            params:{playerNo:1,handNo:1}
+        })
+            .then(
+                res => {
+                    console.log("double success");
+                }
+            )
+            .catch(
+                res => {
+                    console.log("double fail");
+                }
+            )
+    }
+    nextDraw = ()=> {
+        console.log("next player");
+        let currentPlayer = this.state.currentPlayer;
+        if(this.state.currentPlayer <= this.state.playerNumber){
+            this.setState(
+                {
+                    currentPlayer: this.state.currentPlayer + 1
+                })
+        }
+        this.forceUpdate();
+        console.log(this.state.currentPlayer);
+    }
     render() {
         return (
             <div className="App">
                 <div className="Player-part">
                     Player Page
+                    <div className="Player-Info">
+                        <Button className = "rule" type = "primary">Rule</Button>
+                    </div>
+                    <div className="Player-Info">
+                        Player 1's Hand 1's Turn:
+                        <br/>
+                            Current bet is: {this.state.betList[0][0]}
+                    </div>
+                    <div className="All=Card">
+                        {this.state.cardInfo[this.state.currentPlayer - 1][0].map(this.displayCard)}
+                    </div>
+                    <div className="Choice-Part">
+                        <div className="Hint-Message">
+                            Choose the operation you want to do:
+                        </div>
+                        <div className="Choice-Buttons">
+                            <div className="Each-Button">
+                            <Button type="primary" onClick={this.doubleBet}>Double</Button>
+                            </div>
+                            <div className="Each-Button">
+                            <Button type="primary" onClick={this.drawCard}>Draw</Button>
+                            </div>
+                            <div className="Each-Button">
+                            <Button type="primary" onClick={this.nextDraw}>Pass</Button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="Information-part">
                     other information
+                    <div className="Opponent-Info">
+                        <div className="Each-Opponent">
+                            <div className="Opponent-Info">
+                                Player 2's Hand 2:
+                            </div>
+                            <div className="Total-Card">
+                                <div className="Every-Card">
+                                    <img width={70} height={110}class = "op-img" src={require("./Card/Clover 1.jpg")}>
+                                    </img>
+                                </div>
+                                <div className="Every-Card">
+                                    <img  width={70} height={110} src={require("./Card/Clover 1.jpg")}>
+                                    </img>
+                                </div>
+                                <div className="Every-Card">
+                                    <img width={70} height={110} src={require("./Card/Clover 1.jpg")}>
+                                    </img>
+                                </div>
+                                <div className="Every-Card">
+                                    <img width={70} height={110} src={require("./Card/Clover 1.jpg")}>
+                                    </img>
+                                </div>
+                                <div className="Every-Card">
+                                    <img width={70} height={110} src={require("./Card/Clover 1.jpg")}>
+                                    </img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Divider className="divider"></Divider>
                 </div>
             </div>
         );
